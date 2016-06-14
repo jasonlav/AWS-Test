e062c073ef2e44971c894124cf9e2c5b786dc431
jasonlav <jasonlavorante@Jasons-MacBook-Pro.local>
2016-06-14 16:50:41 -0600

    aa

--- s3.php
+++ s3.php
@@ -5,11 +5,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$client = S3Client::factory(array(
+    'key'    => getenv('s3_key'),
+    'secret' => getenc('s3_secret')
+));
+
 $client->registerStreamWrapper();
 
 $file = "testing";
 
-if(file_put_contents('s3://curious-example/'.getenv('s3').'/test.txt', $file)){
+if(file_put_contents('s3://curious-example/test.txt', $file)){
     echo "success";
 } else {
     echo "fail";
