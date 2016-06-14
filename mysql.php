f28e234c1268ae2dbc104babb63bf0d030f47f60
jasonlav <jasonlavorante@Jasons-MacBook-Pro.local>
2016-06-14 15:50:50 -0600

    a

---
+++ mysql.php
@@ -0,0 +1,8 @@
+<?php
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname='.getenv('mysql_database'), getenv('mysql_user'), getenv('mysql_password'));
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
