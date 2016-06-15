3d066299e2b729b8ba9fb0013582582cf42144c9
jasonlav <jasonlavorante@Jasons-MacBook-Pro.local>
2016-06-15 09:11:58 -0600

    a

--- s3.php
+++ s3.php
@@ -3,23 +3,27 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require 'aws/aws-autoloader.php';
+require 'aws.phar';
 
 use Aws\S3\S3Client;
+use Aws\Common\Credentials\Credentials;
 
-/*
-$client = S3Client::factory(array(
-    'key'    => getenv('s3_key'),
-    'secret' => getenv('s3_secret')
+$credentials = new Credentials(getenv('s3_key'), getenv('s3_access'));
+
+// Instantiate the S3 client with your AWS credentials
+$s3Client = S3Client::factory(array(
+    'version' => 'latest',
+    'region' => 'us-east-1',
+    'credentials' => $credentials
 ));
 
-$client->registerStreamWrapper();
-
-$file = "testing";
-
-if(file_put_contents('s3://curious-example/test.txt', $file)){
-    echo "success";
-} else {
-    echo "fail";
+try {
+    $s3Client->putObject(array(
+        'Bucket' => 'curious-example',
+        'Key' => 'test2.txt',
+        'Body' => 'testing',
+        'ACL' => 'public-read'
+    ));
+} catch (Aws\Exception\S3Exception $e) {
+    echo "There was an error uploading the file.\n";
 }
-*/
